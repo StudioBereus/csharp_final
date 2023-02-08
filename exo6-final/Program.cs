4fede614f09046e6d3ea9b245764e58e662f3b2b
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-08 10:31:52 +0100

    Add files via upload

---
+++ exo6-final/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exo6_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer la distance ainsi que son unite de mesure (km / mi");
+            string[] rep = Console.ReadLine().Split(' ');
+            if(double.TryParse(rep[0], out double dist))
+            {
+                double res;
+                if (rep[1] == "km"){
+                    res = dist * 1.609;
+                    Console.WriteLine(res);
+                }
+                else if(rep[1]=="mi"){
+                    res = dist / 1.609;
+                    Console.WriteLine(res);
+                }
+                else{
+                    Console.WriteLine("Erreur de frappe");
+                }
+            }
+        }
+    }
+}
