e46ff60ee0d95c8cb81808f38c7c8dacd01aa5eb
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-08 10:29:33 +0100

    Add files via upload

---
+++ exo1-final/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exo1_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer autant de chiffre que vous le souhaitez, separes d'un espace");
+            string[] ans = Console.ReadLine().Split(' ');
+            if (ans.Length > 2)
+            {
+                double sum = 0;
+                for (int i = 0; i < ans.Length; i++)
+                {
+                    if (double.TryParse(ans[i], out double number))
+                    {
+                        sum += number;
+                    }
+                }
+                double moyenne = sum / ans.Length;
+                Console.WriteLine("La moyenne est de : " + moyenne);
+            }
+            else
+            {
+                Console.WriteLine("Veuillez entrer au moins deux chiffres");
+            }
+        }
+    }
+}
---
+++ exo3-final/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exo3_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer le rayon de la sphère ainsi que l'angle de celui-ci, separes d'un espace");
+            string[] ans = Console.ReadLine().Split(' ');
+            if (double.TryParse(ans[0], out double rayon ) && double.TryParse(ans[1], out double angle)){
+                double aire = (Math.PI * Math.Pow(rayon, 2) * angle) / 360;
+                Console.WriteLine("L'aire du secteur est de : " + aire);
+            }
+        }
+    }
+}
