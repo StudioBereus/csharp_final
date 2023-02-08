557f6c0aeb3c9153637482440de50df88a5b201e
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-08 10:36:01 +0100

    Add files via upload

--- exo2-final/Program.cs
+++ exo2-final/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 
-namespace exo10_final
+namespace exo2_final
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-           
+            Console.WriteLine("Veuillez entrer le rayon de votre sphère");
+            double.TryParse(Console.ReadLine(), out double rayon);
+            if (rayon > 0){
+                double aire = 4 * Math.PI * Math.Pow(rayon, 2);
+                double volume = (4 / 3) * Math.PI * Math.Pow(rayon, 3);
+                Console.WriteLine("Aire : " + aire + "\nVolume : " + volume);
+            }
+            else{
+                Console.WriteLine("Sphere inexistante !");
+            }
         }
     }
 }
