90c22e682f46e60346c0efed292033f2ce7bbc52
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-08 10:32:45 +0100

    Add files via upload

---
+++ exo7-final/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exo7_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer la temperature puis son unite de mesure, separes d'une virgule (C/F)");
+            string[] rep = Console.ReadLine().Split(' ');
+            if(double.TryParse(rep[0], out double temp)){
+                double converted;
+                if (rep[1] == "C")
+                {
+                    converted = (temp * (9 / 5)) + 32;
+                    Console.WriteLine(converted);
+                }
+                else if (rep[1] == "F")
+                {
+                    converted = (temp - 32) * 5 / 9;
+                    Console.WriteLine(converted);
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise entree");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ exo8-final/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace exo8_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jeu de la fourchette avec l'objet random
+
+            //Instantier l'objet random
+            Random rng = new Random();
+
+            //Autres variables
+            int nOrdi, nUtilisateur, nbTentatives = 0, min = 0, max = 100;
+
+            //Définir le chiffre au hasard "choisi" par l'ordi, entre 0 et 100
+            nOrdi = rng.Next(0, 101);
+            Console.WriteLine(nOrdi);
+            do
+            {
+                Console.WriteLine("Veuillez entrer un nombre " + min + " et " + max);
+                if (int.TryParse(Console.ReadLine(), out nUtilisateur))
+                {
+                    if (nUtilisateur == nOrdi)
+                    {
+                        Console.WriteLine("Gagne ! \nNombre de tentatives : " + nbTentatives);
+                    }
+                    else if (nUtilisateur > nOrdi)
+                    {
+                        Console.WriteLine("C'est moins !");//Redefinir la borne max
+                        max = nUtilisateur;
+                        nbTentatives++;
+                    }
+                    else if (nUtilisateur < nOrdi)
+                    {
+                        Console.WriteLine("C'est plus !");//Redefinir la borne minimale
+                        min = nUtilisateur;
+                        nbTentatives++;
+                    }
+                }
+
+            } while (nUtilisateur != nOrdi);
+            //Donner la consigne
+            
+            //Vérfier que l'entree de l'utilisateur est bien un chiffre
+            
+        }
+    }
+}
