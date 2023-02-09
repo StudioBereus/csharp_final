ee34921f7b9c040f7c9ae09c7055be2a0ec3cac2
Chems Hoflack <72741076+StudioBereus@users.noreply.github.com>
2023-02-09 10:20:20 +0100

    Add files via upload
    ajout de l'exercice 11 et 12

---
+++ exo11/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace exo11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string rep = "";
+            bool textRestart = false, reading = false, jonc = false;
+
+
+            Console.WriteLine("Merci de répondre aux questions par par O ou N. Tapez restart pour recommencer a tout moment. \nFera-t-il beau demain?");
+            rep = Console.ReadLine();
+            if (rep == "O")
+            {
+                Console.WriteLine("Je n'ai pas utilise ma bicyclette depuis quelques mois, est ce qu'elle fonctionne ?");
+                rep = Console.ReadLine();
+                if (rep == "O")
+                {
+                    jonc = true;
+                }
+                else if (rep == "N")
+                {
+                    Console.WriteLine("Alors je dois aller chez le garagiste pour le faire réparer. \n La reparation est-elle immédiate ?");
+                    rep = Console.ReadLine();
+                    if (rep == "O")
+                    {
+                        jonc = true;
+                    }
+                    else if (rep == "N")
+                    {
+                        Console.WriteLine("Alors j'irais a pied ceuillir des joncs");
+                    }
+                    else
+                    {
+                        textRestart = true;
+                    }
+                }
+                else
+                {
+                    textRestart = true;
+                }
+            }
+            else if (rep == "N")
+            {
+                Console.WriteLine("Alors je consacrerais ma journee a la lecture. J'aimerais lire le tome 1 de game of thrones, l'ai-je dans ma bibliotheque ?");
+                rep = Console.ReadLine();
+                if (rep == "O")
+                {
+                    reading = true;
+                }
+                else if (rep == "N")
+                {
+                    Console.WriteLine("Alors je dois aller l'emprunter a la bibliotheque. Est-il disponible ?");
+                    rep = Console.ReadLine();
+                    if (rep == "O")
+                    {
+                        reading = true;
+                    }
+                    else if (rep == "N")
+                    {
+                        Console.WriteLine("Alors j'emprunterais un roman policier.");
+                        reading = true;
+                    }
+                    else
+                    {
+                        textRestart = true;
+                    }
+
+                }
+                else
+                {
+                    textRestart = true;
+                }
+            }
+            else { textRestart = true; }
+
+            if (textRestart) { Console.WriteLine("Mauvaise entree ! Tapez restart pour recommencer"); }
+            if (reading) { Console.WriteLine("J'ai mon livre qui me convient, je m'installe sur le fauteil"); }
+
+            if (jonc) { Console.WriteLine("Je vais ceuillir des joncs au bord de l'etang"); }
+        }
+    }
+}
---
+++ exo12/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace exo12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbUtilisateur = 0, nbPc, victoryUt = 0, victoryPc = 0, matchNull=0, res;
+            Random rng = new Random();
+            Console.WriteLine("Jeu du 0-1-2");
+            while (victoryPc < 10 && victoryUt < 10 || nbUtilisateur < 0)
+            {
+                Console.WriteLine("\nVeuillez entrer un chiffre entre 0 et 2");
+                int.TryParse(Console.ReadLine(), out nbUtilisateur);
+                nbPc = rng.Next(0, 3);
+                
+                if (nbPc > nbUtilisateur)
+                {
+                    Console.WriteLine(nbPc);
+                    res = nbPc - nbUtilisateur;
+                    if (res == 0)
+                    {
+                        matchNull++;
+                        Console.WriteLine("Match nul ! " + victoryPc+" "+victoryUt+" "+matchNull);
+                    }
+                    else if (res == 1)
+                    {
+                        victoryUt++;
+                        Console.WriteLine("Utilisateur gagne ! "+ victoryPc + " " + victoryUt + " " + matchNull);
+                    }
+                    else if (res == 2)
+                    {
+                        victoryPc++;
+                        Console.WriteLine("Pc gagne ! "+ victoryPc + " " + victoryUt + " " + matchNull);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(nbPc);
+                    res = nbUtilisateur - nbPc;
+                    if (res == 0)
+                    {
+                        matchNull++;
+                        Console.WriteLine("Match nul ! " + victoryPc + " " + victoryUt + " " + matchNull);
+                    }
+                    else if (res == 1)
+                    {
+                        victoryPc++;
+                        Console.WriteLine("Pc gagne ! " + victoryPc + " " + victoryUt + " " + matchNull);
+                    }
+                    else if (res == 2)
+                    {
+                        victoryUt++;
+                        Console.WriteLine("Utilisateur gagne ! " + victoryPc + " " + victoryUt + " " + matchNull);
+                    }
+                }
+            }
+            Console.WriteLine("Victoires Utilisateur : " + victoryUt + "\n Victoires PC : " + victoryPc + "Matchs nuls : " + matchNull);
+        }
+    }
+}
